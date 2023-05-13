eeecc89f362b8ae48dc76d23ee1a1752afaab418
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2023-05-13 23:52:18 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -22,7 +22,28 @@ Post request json file structure
 const registerUser =async (req, res) => {
 
     //Write you code here
+     const name = req.body.name;
+    const email  = req.body.email;
+    const password = req.body.password;
+    const DOB = req.body.DOB;
 
+
+    // Generate a salt to be used for hashing
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    var newuser = {
+        "name":name,
+        "email":email,
+        "password": hashedPassword,
+        "DOB":DOB
+    };
+    users.create(newuser).then((user) => {
+        res.send(user._id);
+    })
+    .catch((error) => {
+        res.status(404).send(error.message);
+    });
 }
 
 module.exports = { registerUser };
